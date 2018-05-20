5ea1d765e2db416525ca2e31f9728ed78e62511d
sai sharan <s.sharan2008@gmail.com>
2018-05-20 16:36:44 -0400

    added animations and methods

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, style, transition,animate,keyframes,query, stagger} from '@angular/animations'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations:[
+      trigger('goals',[
+            transition('* => *',[
+              query(':enter',style({opacity:0}),{optional:true}),
+
+              query(':enter',stagger('300ms', [
+                  animate('.6s ease-in',keyframes([
+                    style({opacity:0,transform:'translateY(-75%)',offset:0}),
+                    style({opacity:.5,transform:'translateY(35px)',offset:.3}),
+                    style({opacity:0,transform:'translateY(0)',offset:1}),
+                    
+                  ]))]),{optional:true})
+            ])   
+      ])
+  ]
 })
 export class HomeComponent implements OnInit {
 itemCount:number = 4;
 btnText:string = 'Add an item'
 goalText:string = 'My first life goal';
+goals = ['My first life goal', 'Climbing a mountain', 'world tour'];
   constructor() { }
 
   ngOnInit() {
+    // life cycle hook initiated when app loads
+    this.itemCount = this.goals.length;
+  }
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+  removeItem(i){
+    this.goals.slice(i, 1);
   }
 
 }
