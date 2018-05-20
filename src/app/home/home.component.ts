0b6f0f5a9a8e495f11b0105fdbeb0c3ab9ba79cd
sai sharan <s.sharan2008@gmail.com>
2018-05-20 13:05:43 -0400

    added binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+itemCount:number = 4;
+btnText:string = 'Add an item'
+goalText:string = 'My first life goal';
   constructor() { }
 
   ngOnInit() {
